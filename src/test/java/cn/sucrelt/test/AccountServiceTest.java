fa02438f60102bf37e8fa800b1011d9978cc2a1f
tangtangsama <1462339680@qq.com>
2020-09-19 17:07:24 +0800

    测试方法

---
+++ src/test/java/cn/sucrelt/test/AccountServiceTest.java
@@ -0,0 +1,58 @@
+package cn.sucrelt.test;
+
+import cn.sucrelt.domain.Account;
+import cn.sucrelt.service.AccountService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * @description:使用JUnit单元测试测试配置内容
+ * @author: sucre
+ * @date: 2020/09/19
+ * @time: 16:39
+ */
+public class AccountServiceTest {
+
+    ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+    AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+
+    @Test
+    public void testFindAllAccount() {
+        List<Account> accounts = accountService.findAllAccount();
+        for (Account account :
+                accounts) {
+            System.out.println(account.toString());
+        }
+    }
+
+    @Test
+    public void testFindAccountById() {
+        Account account = accountService.findAccountById(1);
+        System.out.println(account);
+    }
+
+    @Test
+    public void testSaveAccount() {
+        Account account = new Account();
+        account.setName("测试用户");
+        account.setMoney(12345f);
+
+        accountService.saveAccount(account);
+    }
+
+    @Test
+    public void testUpdateAccount() {
+        Account account = accountService.findAccountById(4);
+        account.setMoney(23456f);
+        accountService.updateAccount(account);
+    }
+
+    @Test
+    public void testDeleteAccount() {
+        accountService.deleteAccount(4);
+    }
+
+}
